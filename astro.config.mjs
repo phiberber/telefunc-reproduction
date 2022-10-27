176af040ed6a3c526a1d82e8aade32bcea45ef07
Philippe B <phiberberian@gmail.com>
2022-10-27 10:54:27 -0300

    remove solid and update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,10 @@ import { defineConfig } from 'astro/config';
 import { telefunc } from 'telefunc/vite'
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [telefunc()],
         build: { target: 'esnext' },
         appType: 'mpa',
-    },
-    integrations: [
-        solidJs(),
-    ]
+    }
 });
